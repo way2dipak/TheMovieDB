9302ffa641f7829a07ffafb622da923b1096cd30
DS <dipak.singh@embibe.com>
2022-10-26 10:05:16 +0530

    config setup

--- TheMovieDBNetwork/TheMovieDBNetwork/Base/APIBase.swift
+++ TheMovieDBNetwork/TheMovieDBNetwork/Base/APIBase.swift
@@ -10,35 +10,38 @@ public struct APIBase {
 }
 
 public enum Env {
+    case local
     case dev
     case prod
     
     public var url: String {
         switch self {
+        case .local:
+            return "http://localhost:8000/v1/"
         case .dev:
             return "http://moviedbnodejs.herokuapp.com/v1/"
         case .prod:
-            return "http://35.173.249.132:8000"
+            return "http://35.173.249.132:8000/v1/"
         }
     }
     
     public var imgUrl: String {
         switch self {
-        case .dev, .prod:
+        case .local, .dev, .prod:
             return "http://image.tmdb.org/t/p/"
         }
     }
     
     public var videoUrl: String {
         switch self {
-        case .dev, .prod:
+        case .local, .dev, .prod:
             return "https://www.youtube.com/"
         }
     }
     
     public var token: String {
         switch self {
-        case .dev, .prod:
+        case .local, .dev, .prod:
             return APIKEY
         }
     }
